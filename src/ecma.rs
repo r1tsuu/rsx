22cf8f5661ab7c0db40b1a807841ecc3078dc9ca
Sasha <64744993+r1tsuu@users.noreply.github.com>
2026-01-14 04:01:02 +0200

    te

--- src/ecma.rs
+++ src/ecma.rs
@@ -1,46 +1,109 @@
-use crate::core::{ExecutionContext, Object, ObjectRef};
-
-pub trait JSModule<'exec> {
-    fn name() -> &'static str;
-    fn create(exec: &'exec mut ExecutionContext<'exec>) -> Self;
-}
-
-pub trait JSClass<'exec> {
-    fn name(&self) -> &str;
-    fn prototype(&mut self, exec: &'exec mut ExecutionContext<'exec>) -> ObjectRef<'exec>;
-    fn constructor(&mut self, exec: &'exec mut ExecutionContext<'exec>) -> ObjectRef<'exec>;
-}
+use crate::core::{Object, ObjectRef};
+use std::sync::OnceLock;
 
 pub struct ObjectClass<'exec> {
     _prototype: Option<ObjectRef<'exec>>,
     _constructor: Option<ObjectRef<'exec>>,
 }
 
-impl<'exec> JSClass<'exec> for ObjectClass<'exec> {
-    fn name(&self) -> &str {
-        return "Object";
+static OBJECT_CLASS: OnceLock<ObjectClass<'static>> = OnceLock::new();
+
+impl<'exec> ObjectClass<'exec> {
+    fn name() -> &'static str {
+        "Object"
     }
 
-    fn prototype(&mut self, exec: &'exec mut ExecutionContext<'exec>) -> ObjectRef<'exec> {
-        self._prototype.clone().unwrap_or_else(|| {
-            let _prototype = Object::new().build();
-            self._prototype = Some(_prototype.clone());
-            _prototype
+    fn instance() -> &'static ObjectClass<'static> {
+        OBJECT_CLASS.get_or_init(|| ObjectClass {
+            _prototype: None,
+            _constructor: None,
         })
     }
 
-    fn constructor(&mut self, exec: &'exec mut ExecutionContext<'exec>) -> ObjectRef<'exec> {
-        self._constructor.clone().unwrap_or_else(|| {
-            let _constructor = Object::new().build();
-            self._constructor = Some(_constructor.clone());
+    pub fn prototype() -> ObjectRef<'exec> {
+        let instance = Self::instance();
 
-            exec.subscribe_to_class_initialization("Function", move |class, exec| {
-                let proto = class.prototype(exec);
+        instance
+            ._prototype
+            .unwrap_or_else(|| {
+                let prototype = Object::new().build();
+                instance._prototype = Some(prototype.clone());
+                prototype
+            })
+            .clone()
+    }
 
-                _constructor.borrow_mut().set_prototype(proto.clone());
-            });
+    pub fn constructor() -> ObjectRef<'exec> {
+        let instance = Self::instance();
 
-            _constructor
+        instance
+            ._constructor
+            .unwrap_or_else(|| {
+                let constructor = Object::new().build();
+                instance._constructor = Some(constructor.clone());
+                constructor
+            })
+            .clone()
+    }
+
+    pub fn init(function_prototype: ObjectRef<'exec>) {
+        let instance = Self::instance();
+
+        instance._constructor.inspect(|constructor| {
+            constructor.borrow_mut().set_prototype(function_prototype);
+        });
+    }
+}
+
+pub struct FunctionClass<'exec> {
+    _prototype: Option<ObjectRef<'exec>>,
+    _constructor: Option<ObjectRef<'exec>>,
+}
+
+static FUNCTION_CLASS: OnceLock<FunctionClass<'static>> = OnceLock::new();
+
+impl<'exec> FunctionClass<'exec> {
+    fn name() -> &'static str {
+        "Function"
+    }
+
+    fn instance() -> &'static FunctionClass<'static> {
+        FUNCTION_CLASS.get_or_init(|| FunctionClass {
+            _prototype: None,
+            _constructor: None,
         })
+    }
+
+    pub fn prototype() -> ObjectRef<'exec> {
+        let instance = Self::instance();
+
+        instance
+            ._prototype
+            .unwrap_or_else(|| {
+                let prototype = Object::new().build();
+                instance._prototype = Some(prototype.clone());
+                prototype
+            })
+            .clone()
+    }
+
+    pub fn constructor() -> ObjectRef<'exec> {
+        let instance = Self::instance();
+
+        instance
+            ._constructor
+            .unwrap_or_else(|| {
+                let constructor = Object::new().with_prototype(Self::prototype()).build();
+                instance._constructor = Some(constructor.clone());
+                constructor
+            })
+            .clone()
+    }
+
+    pub fn init(object_prototype: ObjectRef<'exec>) {
+        let instance = Self::instance();
+        instance._constructor.inspect(|constructor| {
+            constructor.borrow_mut().set_prototype(object_prototype);
+        });
     }
 }
